daf58ad8a9ed04671dcf6d3b990391ea6d7a511c
XyanZhang <1743067175@qq.com>
2023-05-09 21:47:07 +0800

    feat: 解析命令行参数

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,7 +1,28 @@
-#!/usr/bin/env node 
+// #!/usr/bin/env node 
 // 表示用node执行该文件
 
-const initM = require('@cli-ken/init')
+const lib = require('@cli-ken/init')
 
-initM.init('hello');
-console.log('I\'m cli ken')
+// initM.init('hello');
+// console.log('I\'m cli ken')
+
+// 注册一个命令 init
+const agrv = require('process').argv;
+// console.log(agrv);
+// 获取参数
+let command = agrv[2];
+console.log(command);
+if(command) {
+  command = command.replace('--', '');
+  if(lib[command]) {
+    lib[command](agrv[3]); // 根据参数执行命令
+    // ken-cli init hello
+    // 打印 I'm init ken from init hello
+  }else {
+    console.log('command not found');
+  }
+}else {
+  console.log('no command');
+}
+
+// 实现参数解析
